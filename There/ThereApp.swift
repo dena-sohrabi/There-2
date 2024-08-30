d19b202af603d710187b038b47dbfa210c66d3bc
Dena Sohrabi <denasohrabi62@gmail.com>
2024-08-30 20:17:30 +0330

    Initial Commit

---
+++ There/ThereApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThereApp.swift
+//  There
+//
+//  Created by Dena Sohrabi on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ThereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
